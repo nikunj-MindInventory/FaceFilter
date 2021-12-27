b8769636ef963aa2acf65927f31c04533b8c3c8e
Nikunj Prajapati <nikunj.prajapati@mindinventory.com>
2021-12-27 11:03:49 +0530

    Initial Commit

---
+++ FaceEffects-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceEffects-Demo
+//
+//  Created by mac-0002 on 27/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
